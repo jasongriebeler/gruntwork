9c13e5be3e2a17de68762870904d99dfefb6c647
Jason Griebeler <jason.griebeler@isobar.com>
2019-08-11 13:03:36 -0500

    navigating next links.

--- src/gruntwork.js
+++ src/gruntwork.js
@@ -25,56 +25,73 @@ const workouts = db.ref("workouts");
     await page.click("button[name='login']");
     await page.goto("https://www.gruntworkout.com/category/workouts/");
 
-
-
     let modal = await page.$("div.mc-closeModal", );
-    console.log("MODAL: " + modal);
     if(modal){
         await page.waitFor("div.mc-closeModal", { visible: true })
             .then(element => element.click());
     }
 
     // await page.goto("file:///Users/jgriebeler/Downloads/Grunt%20Work%20Workouts%20%E2%80%A2%20Grunt%20Work.htm");
+    // await page.goto("file:///Users/jgriebeler/Downloads/Grunt%20Work%20Workouts%20%E2%80%A2%20Grunt%20Work%20-last.htm");
 
-    let workoutLinks = await page.$$eval("h3.entry-title > a", anchors => {
-        return anchors.map(a => {
-            return {
-                title: a.innerText,
-                link: a.href
-            };
-        })
-    });
-    // let workoutLinks = [
-    //     {
-    //         title: 'W/O – 08.10.19',
-    //         link: 'file:///Users/jgriebeler/Downloads/W_O%20-%2008.16.19%20%E2%80%A2%20Grunt%20Work.html'
-    //     }
-    // ];
-    workoutLinks = workoutLinks.filter(workoutLink => !childExists(workouts, nodeName(workoutLink.title))); // remove anything that already exists in firebase
-
-    for(let entry of workoutLinks) {
-        console.log(entry);
-        await page.goto(entry.link);
-        entry.content = await page.$$eval("div.entry-content > p", paragraphs => {
-            return paragraphs.map(p => {return {
-                innerHtml: p.innerHTML,
-                innerText: p.innerText
-            }
-            });
-        });
-        entry.metaTags = await page.$$eval("div.post_tags a", anchors => {
-            return anchors.map(a => a.innerText);
-        });
-        workouts.child(nodeName(entry.title)).set(entry);
-    }
+    let advance = true;
+    do {
+        await logEntries();
+        let nextPage = await page.$("ul.page-numbers li a.next");
+        if(nextPage) {
+            await nextPage.click();
+            await page.waitForNavigation();
+        }
+        else
+            advance = false;
+    } while(advance);
 
     app.delete()
         .then(browser.close());
+
+    async function logEntries(){
+        // let workoutLinks = await page.$$eval("h3.entry-title > a", anchors => {
+        //     return anchors.map(a => {
+        //         return {
+        //             title: a.innerText,
+        //             node: nodeName(a.innerText),
+        //             link: a.href
+        //         };
+        //     })
+        // });
+        let workoutLinks = [
+            {
+                title: 'W/O – 08.10.19',
+                link: 'file:///Users/jgriebeler/Downloads/W_O%20-%2008.16.19%20%E2%80%A2%20Grunt%20Work.html',
+                node: nodeName('W/O – 08.10.19')
+            }
+        ];
+
+        const entriesExist = await Promise.all(workoutLinks.map(l => childExists(workouts, l.node)));
+        workoutLinks = workoutLinks.filter(l => !entriesExist.find(e => e.node = l.node).exists );
+
+        for(let entry of workoutLinks) {
+            console.log(entry);
+            await page.goto(entry.link);
+            entry.content = await page.$$eval("div.entry-content > p", paragraphs => {
+                return paragraphs.map(p => {return {
+                    innerHtml: p.innerHTML,
+                    innerText: p.innerText
+                }
+                });
+            });
+            entry.metaTags = await page.$$eval("div.post_tags a", anchors => {
+                return anchors.map(a => a.innerText);
+            });
+            workouts.child(nodeName(entry.title)).set(entry);
+        }
+    }
 })();
 
 async function childExists(ref, child){
-    return await ref.child(child).once("value")
+    let exists = await ref.child(child).once("value")
         .then(snapshot => snapshot.val() !== null);
+    return { node: child, exists: exists };
 }
 
 function nodeName(input){
